0da89ff338be3469a7f1f4d80911548c5e246496
Xavi <xcalduch@gmail.com>
2023-02-26 20:48:05 +0100

    New IP Aws

--- src/aplication/api/favs.api.js
+++ src/aplication/api/favs.api.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
 export const createFavRequest = async (fav)=> {
-  return await axios.post('http://localhost:4000/api/createfav', fav )
+  return await axios.post('http://13.37.57.122/api/createfav', fav )
 }
 
 export const FavsUserRequest = async (userMail)=> {
-    return await axios.get(`http://localhost:4000/api/userfavs/${userMail}`);  
+    return await axios.get(`http://13.37.57.122:4000/api/userfavs/${userMail}`);  
  }
 
 export const delFavsUserRequest = async (idMovie, mail)=> {
-   return await axios.delete(`http://localhost:4000/api/deletemovie/${idMovie}/${mail}`);  
+   return await axios.delete(`http://13.37.57.122:4000/api/deletemovie/${idMovie}/${mail}`);  
 }
 
 
